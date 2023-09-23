cfff08976b6c04ceb3f9a5e2ae28faa2bb09a651
wermi <32251376+wermipls@users.noreply.github.com>
2023-09-24 01:04:58 +0200

    iNiTiAL COMMiT

---
+++ conv-scripts/deco.py
@@ -0,0 +1,369 @@
+import argparse
+from bs4 import BeautifulSoup
+from dataclasses import dataclass
+from enum import IntEnum
+from datetime import datetime
+from dateutil import parser as dateparser
+from typing import List
+import re
+import mysql.connector
+import traceback
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input")
+parser.add_argument("-host", required=True)
+parser.add_argument("-u", required=True)
+parser.add_argument("-p", required=True)
+parser.add_argument("-db", required=True)
+parser.add_argument("-t", required=True)
+
+args = parser.parse_args()
+
+f = open(args.input, "r")
+html_doc = f.read()
+f.close()
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+authors = { }
+
+author_id = 0
+
+class UserType(IntEnum):
+    NORMAL = 1
+    WEBHOOK = 2
+    BOT = 3
+
+@dataclass
+class Author:
+    uid: int
+    name: str = 'Anonymous'
+    avatar_url: str | None = None
+    user_type: UserType = UserType.NORMAL
+
+    def __repr__(self):
+        return f'[{self.uid}] [{self.user_type}] {self.name}'
+
+@dataclass
+class Attachment:
+    attach_type: str
+    url: str
+
+@dataclass
+class Embed:
+    author: str | None = None
+    author_url: str | None = None
+    title: str | None = None
+    title_url: str | None = None
+    description: str | None = None
+    special_container_url: str | None = None
+
+    def __repr__(self):
+        return f"        {self.author}\n        {self.title}\n        {self.description}"
+
+@dataclass
+class Message:
+    uid: int
+    author_id: int
+    content: str | None
+    date: datetime
+    replies_to: int | None = None
+    sticker: str | None = None
+    attachments: List[Attachment] | None = None
+    embed: Embed | None = None
+
+def parse_datetime(dt: str | None) -> datetime:
+    if not dt:
+        return dateparser.parse("1970-01-01 00:00")
+
+    return dateparser.parse(dt)
+
+def try_find_timestamp(chatmsg):
+    # case 1: chatlog__timestamp in header
+    ts = chatmsg.find(class_='chatlog__timestamp')
+    if ts:
+        return ts.a.contents[0]
+
+    # case 2: chatlog__short-timestamp, full timestamp in title
+    ts = chatmsg.find(class_='chatlog__short-timestamp')
+    if ts:
+        return ts['title']
+    else:
+        return None
+
+# finds id or None if not present
+def find_author_id(chatmsg) -> int | None:
+    a = msg.find(class_='chatlog__author')
+    if a:
+        return int(a['data-user-id'])
+    else:
+        return None
+
+# returns author or None if not present
+def find_author(chatmsg) -> Author | None:
+    a = msg.find(class_='chatlog__author')
+    if a:
+        author = Author(
+            uid = int(a['data-user-id']),
+            name = a.contents[0]
+        )
+    else:
+        return None
+    
+    a = msg.find(class_='chatlog__avatar')
+    if a:
+        author.avatar_url = a['src']
+
+    bot = msg.find(class_='chatlog__bot-label')
+    if bot:
+        author.user_type = UserType.BOT
+    else:
+        author.user_type = UserType.NORMAL
+
+    return author
+
+def parse_markdown(content):
+    # mentions
+    for m in content.find_all(class_='chatlog__markdown-mention'):
+        m.string = f'<{m.string}>'
+        m.unwrap()
+
+    # motes
+    for e in content.find_all(class_='chatlog__emoji'):
+        if 'cdn.discordapp.com/emojis' in e['src']:
+            emote_tag = f':{e["title"]}:'
+            e.replace_with(emote_tag)
+        else:
+            e.replace_with(e['alt'])
+
+    # links
+    for l in content.find_all('a'):
+        l.unwrap()
+
+    # em
+    for em in content.find_all('em'):
+        em.replace_with(f'*{em.string}*')
+
+    # bold
+    for b in content.find_all('b'):
+        b.replace_with(f'**{b.string}**')
+
+    # inline code
+    for c in content.find_all('code'):
+        c.replace_with(f'`{c.string}`')
+
+    content.smooth()
+
+    return content.string
+
+def parse_attachments(chatmsg) -> List[Attachment] | None:
+    attachments = []
+
+    for a in chatmsg.find_all(class_='chatlog__attachment'):
+        media = a.find(class_='chatlog__attachment-media')
+        if not media:
+            continue
+
+        if media.name == 'img':
+            attachments.append(
+                Attachment('image', media['src']))
+        if media.name == 'video':
+            attachments.append(
+                Attachment('video', media.source['src']))
+        if media.name == 'audio':
+            attachments.append(
+                Attachment('audio', media.source['src']))
+
+    if len(attachments) > 0:
+        return attachments
+    else:
+        return None
+
+def parse_embed(chatmsg) -> Embed | None:
+    e = chatmsg.find(class_='chatlog__embed')
+    if not e:
+        return None
+
+    embed = Embed()
+    
+    url = e.find(class_='chatlog__embed-author-link')
+    if url:
+        embed.author_url = url['href']
+
+    author = e.find(class_='chatlog__embed-author')
+    if author:
+        embed.author = author.string
+    
+    title = e.find(class_='chatlog__embed-title')
+    if title:
+        if title.a:
+            embed.title_url = title.a['href']
+        content = title.find(class_='chatlog__markdown-preserve')
+        if content:
+            embed.title = parse_markdown(content)
+
+    desc = e.find(class_='chatlog__embed-description')
+    if desc:
+        content = desc.find(class_='chatlog__markdown-preserve')
+        if content:
+            content = parse_markdown(content)
+            embed.description = content
+
+    return embed
+
+reply_id = re.compile(r"scrollToMessage\(event,'(\d+)'\)")
+
+def parse_message(chatmsg, author_id) -> Message:
+    msg_id = int(chatmsg.parent['data-message-id'])
+
+    new_id = find_author_id(msg)
+    if new_id:
+        author_id = new_id
+        if new_id not in authors:
+            author = find_author(msg)
+            authors[new_id] = author
+
+    timestamp = parse_datetime(try_find_timestamp(msg))
+
+    content = msg.find('span', class_='chatlog__markdown-preserve')
+    if content:
+        content = parse_markdown(content)
+
+    sticker = msg.find(class_='chatlog__sticker')
+    if sticker:
+        sticker = sticker.img['src']
+
+    reply = msg.find(class_='chatlog__reference-link')
+    if reply:
+        reply = reply_id.match(reply['onclick'])
+        if reply:
+            reply = int(reply.group(1))
+
+    attachments = parse_attachments(chatmsg)
+    embed = parse_embed(chatmsg)
+
+    message = Message(
+        msg_id,
+        author_id,
+        content,
+        timestamp,
+        sticker=sticker,
+        attachments=attachments,
+        embed=embed,
+        replies_to=reply)
+
+    return message
+
+msgs = {}
+
+for msg in soup.find_all('div', class_='chatlog__message'):
+    m = parse_message(msg, author_id)
+    author_id = m.author_id
+
+    msgs[m.uid] = m
+
+    if m.replies_to:
+        rep = msgs[m.replies_to]
+        print(f'::: REPLY TO ::: {authors[rep.author_id].name}: {rep.content}')
+
+    if m.content:
+        print(f'[{m.date}] {authors[m.author_id].name}: {m.content}')
+    elif m.sticker:
+        print(f'[{m.date}] {authors[m.author_id].name}: [sticker: {m.sticker}]')
+    else:
+        print(f'[{m.date}] {authors[m.author_id].name}: [no content]')
+
+    if m.attachments:
+        attachtypes = []
+        for a in m.attachments:
+            attachtypes.append(a.attach_type)
+
+        print('Attachments: ' + ', '.join(attachtypes))
+
+    if m.embed:
+        print(m.embed)
+
+for au in authors.items():
+    print(au[1])
+
+
+# mysql sutff
+
+connection = mysql.connector.connect(
+    host=args.host,
+    user=args.u,
+    passwd=args.p,
+    database=args.db)
+
+query = """
+CREATE TABLE IF NOT EXISTS tp_authors (
+    uid BIGINT NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    avatar_url VARCHAR(500),
+    utype INT
+);
+
+CREATE TABLE IF NOT EXISTS tp_channel_oldgeneral (
+    uid BIGINT NOT NULL PRIMARY KEY,
+    author_id BIGINT NOT NULL,
+    content TEXT
+    sticker TEXT
+);
+"""
+
+cursor = connection.cursor()
+cursor.execute(query, multi=True)
+
+query = f"""
+REPLACE INTO {args.t} (uid, author_id, date_sent, content, sticker, attachment, replies_to) 
+VALUES (%s, %s, %s, %s, %s, %s, %s);
+"""
+vals = []
+
+for m in msgs.items():
+    msg = m[1]
+    msg.content = str(msg.content) if msg.content else None
+    msg.sticker = str(msg.sticker) if msg.sticker else None
+    msg.replies_to = str(msg.replies_to) if msg.replies_to else None
+    attachment_id = None
+    if msg.attachments:
+        cursor.execute("SELECT MAX(id) FROM tp_attachments")
+        last_id = cursor.fetchone()
+        last_id = int(last_id[0]) if last_id else 1
+
+        attachment_id = last_id + 1
+
+        for a in msg.attachments:
+            cursor.execute(
+                "INSERT INTO tp_attachments (id, type, url) VALUES (%s, %s, %s)",
+                [attachment_id, a.attach_type, a.url]
+            )
+
+    val = (msg.uid, msg.author_id, 
+        msg.date.strftime('%Y-%m-%d %H:%M:%S'),
+        msg.content, msg.sticker, attachment_id, msg.replies_to)
+    vals.append(val)
+
+try:
+    cursor.executemany(query, vals)
+    connection.commit()
+except Exception as e:
+    traceback.print_exc()
+
+query = """
+INSERT IGNORE INTO tp_authors (uid, name, avatar_url, utype)
+VALUES (%s, %s, %s, %s);
+"""
+vals = []
+
+for au in authors.items():
+    a = au[1]
+    a.name = str(a.name) if a.name else None
+    a.avatar_url = str(a.avatar_url) if a.avatar_url else None
+    vals.append((a.uid, a.name, a.avatar_url, int(a.user_type)))
+
+try:
+    cursor.executemany(query, vals)
+    connection.commit()
+except Exception as e:
+    traceback.print_exc()
---
+++ public/index.php
@@ -0,0 +1,352 @@
+<!doctype html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+:root {
+    --color-bg: #000000;
+    --color-fg: #ffffff;
+    --color-bg-secondary: #202020;
+    --color-fg-secondary: #808080;
+    --color-accent: #ff80d0;
+}
+
+@media (prefers-color-scheme: light) {
+  :root {
+    --color-bg: #ffffff;
+    --color-fg: #000000;
+    --color-bg-secondary: #f2f2f2;
+    --color-fg-secondary: #606060;
+    --color-accent: #e000a0;
+  }
+}
+
+* {
+    box-sizing: border-box;
+}
+
+body {
+    max-width: 800px;
+    padding: 12px;
+    margin: auto;
+    align-items: center;
+    font-family: Helvetica, sans-serif;
+    font-size: 1em;
+    background-color: var(--color-bg);
+    color: var(--color-fg);
+    line-height: 1.2;
+}
+
+
+a, a:hover, a:visited, a:active {
+  color: var(--color-accent);
+}
+
+.msg_avi {
+    width: 40px;
+    border-radius: 8px;
+    overflow: hidden;
+    display: block;
+    margin-left: auto;
+    margin-top: 12px;
+    margin-bottom: 12px;
+    background-color: var(--color-bg-secondary);
+}
+
+.msg_header {
+    grid-column: 2;
+    min-width: 0;
+    padding-top: 12px;
+    margin-top: auto;
+    margin-bottom: 12px;
+}
+
+.msg_side {
+    grid-column: 1;
+    width: 40px;
+    margin-right: 12px;
+}
+
+.msg_user {
+    font-weight: bold;
+    font-size: 1.2em;
+}
+
+.msg_primary {
+    grid-column: 2;
+}
+
+.msg_content {
+    overflow-wrap: anywhere; 
+    min-width: 0;
+}
+
+.msg_element {
+    margin-bottom: 5px;
+}
+
+.msg_date {
+    display: inline-block;
+    margin-left: auto;
+    color: var(--color-fg-secondary);
+}
+
+.msg_time {
+    text-align: right;
+    color: var(--color-fg-secondary);
+    margin: auto;
+}
+
+.msg_sticker {
+    height: 192px;
+    max-width: 192px;
+    border-radius: 8px;
+    background-color: var(--color-bg-secondary);
+}
+
+.msg_attachment {
+    max-height: 350px;
+    max-width: 100%;
+    border-radius: 8px;
+    background-color: var(--color-bg-secondary);
+    margin-right: 8px;
+    margin-bottom: 8px;
+}
+
+.msg_reply {
+    color: var(--color-fg-secondary);
+}
+
+.msg_reply_user {
+    font-weight: bold;
+    color: var(--color-fg);
+}
+
+.msg_reply_ref {
+    color: inherit;
+    text-decoration: none;
+}
+
+.msg_reply_content {
+    display: inline-block;
+    margin-top: 12px;
+    margin-right: auto;
+    padding: 0px 12px;
+    min-width: 0;
+    overflow-wrap: anywhere;
+    color: color-mix(in lch, var(--color-bg) 25%, var(--color-fg));
+    font-style: italic;
+}
+
+.msg {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    direction: ltr;
+}
+
+.msg_ping {
+    display: inline-block;
+    background-color: var(--color-bg-secondary);
+    border-radius: 5px;
+    padding: 2px;
+    font-weight: bold;
+    color: var(--color-accent);
+}
+
+.msg_date_header {
+    overflow: hidden;
+    text-align: center;
+    margin-top: 24px;
+    margin-bottom: 12px;
+    color: color-mix(in lch, var(--color-bg) 25%, var(--color-fg));
+}
+
+.msg_date_header:before,
+.msg_date_header:after {
+    background-color: var(--color-bg-secondary);
+    content: "";
+    display: inline-block;
+    height: 3px;
+    position: relative;
+    vertical-align: middle;
+    width: 50%;
+}
+
+.msg_date_header:before {
+    right: 1em;
+    margin-left: -50%;
+}
+
+.msg_date_header:after {
+    left: 1em;
+    margin-right: -50%;
+}
+
+p {
+    display: block;
+    margin: 0;
+}
+</style>
+</head>
+<body>
+<?php
+mb_internal_encoding("UTF-8");
+mb_http_output("UTF-8");
+
+$start = microtime(true);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+class DiscordParser extends Parsedown
+{
+    function __construct()
+    {
+        $this->InlineTypes['<'] = ['Mention', 'SpecialCharacter'];
+
+        $this->inlineMarkerList .= '<';
+    }
+
+    protected function inlineMention($excerpt)
+    {
+        if (preg_match('/^<@([\w ]+)>/', $excerpt['text'], $matches)) {
+            return array(
+                'extent' => strlen($matches[0]),
+                'element' => array(
+                    'name' => 'span',
+                    'text' => "@" . $matches[1],
+                    'attributes' => array(
+                        'class' => 'msg_ping',
+                    ),
+                ),
+            );
+        }
+    }
+
+    protected $safeLinksWhitelist = array(
+        'http://',
+        'https://',
+    );
+
+    protected function inlineEmailTag($Excerpt) { return; }
+    protected function inlineImage($Excerpt) { return; }
+    protected function inlineUrlTag($Excerpt) { return; }
+}
+
+$config = include("../config.php");
+$parsedown = new DiscordParser();
+$parsedown->setSafeMode(true);
+$parsedown->setBreaksEnabled(true);
+
+$db = new PDO("mysql:host={$config['db_host']};dbname={$config['db_name']};charset=utf8mb4", $config['db_user'], $config['db_pass']);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$select = $db->prepare(
+    "SELECT ch.uid, author_id, date_sent, replies_to, content, sticker, attachment, tp_authors.name, tp_authors.avatar_url
+     FROM {$config['channel_table']} ch
+     INNER JOIN tp_authors
+     ON ch.author_id = tp_authors.uid
+     ORDER BY ch.uid ASC;");
+$select->execute();
+
+$last_uid = 0;
+$last_date = NULL;
+
+while ($row = $select->fetch(PDO::FETCH_ASSOC)) {
+    $msg_id = $row['uid'];
+    $user = $row['name'];
+    $content = $row['content'];
+    $sticker = $row['sticker'];
+    $avi_url = $row['avatar_url'];
+    $attach_id = $row['attachment'];
+    $reply_id = $row['replies_to'];
+    $datetime = new DateTimeImmutable($row['date_sent']);
+    if ($last_date === NULL) {
+        $last_date = $datetime;
+    }
+    $date_delta = $datetime->diff($last_date);
+    $print_date_header = ($datetime->setTime(0, 0)->diff($last_date->setTime(0, 0))->d > 0);
+    $last_date = $datetime;
+
+    $show_author = false;
+    if ($last_uid != $row['author_id']) {
+        $last_uid = $row['author_id'];
+        $show_author = true;
+    }
+    if ($reply_id) {
+        $show_author = true;
+    }
+
+    if ($date_delta->i > 10) {
+        $show_author = true;
+    }
+
+    if ($print_date_header) {
+        $show_author = true;
+        $dh = $datetime->format('l, j F Y');
+        echo "<div class='msg_date_header'>$dh</div>";
+    }
+
+    echo "<div class='msg' id='$msg_id'>";
+    if ($show_author) {
+        echo "<div class='msg_side'><img class='msg_avi' src='$avi_url'></img></div>";
+        echo "<div class='msg_header'><span class='msg_user'>$user </span>";
+        $dts = "<span class='msg_date'>on {$datetime->format('d.m.Y')}</span>";
+
+        if ($reply_id) {
+            $reply_to = $db->query(
+                "SELECT ch.content, a.name FROM {$config['channel_table']} ch
+                 INNER JOIN tp_authors a
+                 ON ch.author_id = a.uid
+                 WHERE ch.uid = $reply_id");
+            $reply_to = $reply_to->fetch();
+            $reply_content = $reply_to['content'];
+            $reply_max = 120;
+            if (mb_strlen($reply_content) > $reply_max) {
+                $reply_content = mb_substr($reply_content, 0, $reply_max) . "...";
+            }
+            echo "<span class='msg_reply'> in response to </span><a class='msg_reply_ref' href='#$reply_id'><span class='msg_reply_user'>{$reply_to['name']} </span>";
+            echo $dts;
+            echo "<br><span class='msg_reply_content'>$reply_content</span></a>";
+        } else {
+            echo $dts;
+        }
+        echo "</div>";
+    }
+    $time = $datetime->format('H:i');
+    echo "<div class='msg_side'><div class='msg_time'>$time</div></div>";
+
+    echo "<div class='msg_primary'>";
+    if ($content) {
+        $content = $parsedown->line($content);
+        echo "<div class='msg_content msg_element'>$content</div>";
+    }
+    if ($attach_id) {
+        foreach ($db->query(
+            "SELECT type, url FROM tp_attachments WHERE id = $attach_id")
+            as $att) {
+                $url = htmlspecialchars($att['url']);
+                switch ($att['type']) {
+                case 'image':
+                    echo "<a href='$url' target='_blank'><img loading='lazy' class='msg_attachment msg_element' src='$url'></img></a>";
+                    break;
+                case 'video':
+                    echo "<video class='msg_attachment msg_element' src='$url'></video>";
+                    break;
+                case 'audio':
+                    echo "<audio class='msg_attachment msg_element' src='$url'></audio>";
+                    break;
+                case 'file':
+                    echo "<div class='msg_attachment msg_element'><a href='$url'>Attachment: $url</a></div>";
+                    break;
+                }
+            }
+    }
+    if ($sticker) {
+        echo "<img class='msg_sticker msg_element' src='$sticker'></img>";
+    }
+    echo "</div>";
+    echo "</div>";
+}
+?>
+</body>
+</html>
