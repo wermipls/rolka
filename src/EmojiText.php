287464b1fd5207059296f1b273da5fd8ab4d2fd6
wermi <32251376+wermipls@users.noreply.github.com>
2023-10-09 09:16:19 +0200

    switch php-twemoji branch, use forked twemoji base

--- src/EmojiText.php
+++ src/EmojiText.php
@@ -5,6 +5,12 @@ use Astrotomic\Twemoji;
 
 class EmojiText extends Twemoji\EmojiText
 {
+    public function __construct(string $text)
+    {
+        $this->text = $text;
+        $this->base('https://cdn.jsdelivr.net/gh/jdecked/twemoji@latest/assets');
+    }
+
     public function toTag(): string
     {
         return $this->replace(
