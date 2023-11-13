01f1dfb20ab71df9ead358fe4bb3f775f66dbf78
wermi <32251376+wermipls@users.noreply.github.com>
2023-11-13 13:26:24 +0100

    refactor bot into a class

--- discord_bot.php
+++ discord_bot.php
@@ -11,6 +11,156 @@ use Discord\Parts\Channel\Channel;
 use rolka\AssetManager;
 use rolka\Context;
 use React\Async;
+use rolka\MediaConverter;
+
+class Bot
+{
+    private Array $channels = [];
+
+    public function __construct(
+        private Context $ctx,
+        private Discord $d,
+        private MediaConverter $conv,
+        private AssetManager $am
+    ) {
+        $this->fetchSyncedChannels();
+
+        $d->on(Event::MESSAGE_CREATE, function ($msg, $d) {
+            $this->mapInsertMessage($msg);
+        });
+
+        $d->on('init', function (Discord $d) {
+            error_log("Hello World!");
+
+            foreach ($this->channels as $id => $channel) {
+                $this->fetchWholeHistory($id);
+            }
+        });
+    }
+
+    public function run()
+    {
+        $this->d->run();
+    }
+
+    private function fetchSyncedChannels()
+    {
+        $q = $this->ctx->db->query("SELECT * FROM channels");
+        $chn = $q->fetchAll();
+
+        foreach ($chn as $c) {
+            if ($sync_id = $c['sync_channel_id']) {
+                $this->channels[$sync_id] = $this->ctx->getChannel($c['id']);
+            }
+        }
+    }
+
+    private function mapInsertMessage(Message $msg)
+    {
+        $ch = $this->channels[$msg->channel_id];
+        if (!$ch) {
+            return;
+        }
+
+        $attachments = [];
+        if ($msg->attachments) {
+            foreach ($msg->attachments as $a) {
+                $asset = $this->am->downloadAsset($a->url);
+                array_push($attachments, $asset);
+            }
+        }
+
+        $embeds = [];
+
+        foreach ($msg->embeds as $e) {
+            $embed_url = $e->video->url;
+            $asset_url = $embed_url ? null : $e->thumbnail->url;
+            if ($asset_url) {
+                $asset = $this->am->downloadAsset($asset_url);
+            }
+
+            $embed = new rolka\Embed(
+                -1,
+                $e->url,
+                $e->type == 'rich' ? 'link' : $e->type,
+                $e->color ? sprintf("#%06x", $e->color) : null,
+                $e->timestamp ? DateTimeImmutable::createFromMutable($e->timestamp) : null,
+                $e?->provider?->name,
+                $e?->provider?->url,
+                $e->footer?->text,
+                $e->footer?->icon_url,
+                $e->author?->name,
+                $e->author?->url,
+                $e->title,
+                $e->url,
+                $e->description,
+                $asset_url ? $this->am->downloadAsset($asset_url) : null,
+                $embed_url
+            );
+            array_push($embeds, $embed);
+        }
+
+        if (!($author = $this->ctx->getAuthor($msg->author->id))) {
+            $author = new rolka\Author(
+                $msg->author->id,
+                $msg->author->displayname,
+                $this->am->downloadAsset($msg->author->avatar)
+            );
+            $this->ctx->insertAuthor($author);
+        }
+
+        $m = new rolka\Message(
+            $ch,
+            $msg->id,
+            $author,
+            DateTimeImmutable::createFromMutable($msg->timestamp),
+            $msg->content,
+            $msg->referenced_message?->id,
+            $msg->sticker_items?->first()?->id,
+            $this->ctx->insertAttachmentGroup($attachments),
+            $this->ctx->insertEmbedGroup($embeds),
+        );
+
+        // hack... should put stuff on a queue
+        foreach ($attachments as $a) {
+            $this->am->generateThumbnail($a->id);
+        }
+
+        $ch->insertMessage($m);
+    }
+
+    private function fetchMessages(Channel $channel, ?int $before): mixed
+    {
+        error_log(__FUNCTION__ .": last id $before");
+        $opts = ['limit' => 100];
+        if ($before) {
+            $opts['before'] = (string)$before;
+        }
+
+        return Async\await($channel->getMessageHistory($opts));
+    }
+
+    private function fetchWholeHistory(int $channel_id)
+    {
+        $ch = $this->d->getChannel($channel_id);
+
+        $last_id = null;
+
+        do {
+            $messages = $this->fetchMessages($ch, $last_id);
+
+            if (!($messages instanceof Collection)) {
+                break;
+            }
+
+            foreach ($messages as $msg) {
+                $this->mapInsertMessage($msg);
+            }
+
+            $last_id = $messages->last()?->id;
+        } while ($last_id);
+    }
+}
 
 $config = include(__DIR__.'/config.php');
 
@@ -23,16 +173,6 @@ $db = new PDO(
 
 $ctx = new Context($db);
 
-$chn = $db->query("SELECT * FROM channels");
-$chn = $chn->fetchAll();
-$mapped_ch = [];
-
-foreach ($chn as $c) {
-    if ($sync_id = $c['sync_channel_id']) {
-        $mapped_ch[$sync_id] = $ctx->getChannel($c['id']);
-    }
-}
-
 $discord_logger = new \Monolog\Logger('discord-php');
 $discord_logger->pushHandler(
     new \Monolog\Handler\StreamHandler('php://stdout', \Monolog\Logger::INFO)
@@ -44,144 +184,12 @@ $d = new Discord([
     'logger' => $discord_logger
 ]);
 
-$conv = new rolka\MediaConverter(
+$conv = new MediaConverter(
     $config['mozjpeg_cjpeg'],
     $config['mozjpeg_jpegtran']
 );
-$am = new rolka\AssetManager($ctx->db, $config['asset_path'], $conv);
+$am = new AssetManager($ctx->db, $config['asset_path'], $conv);
 
-$mapInsertMessage = function (Message $msg, $mapped_ch) use ($am, $ctx)
-{
-    $ch = $mapped_ch[$msg->channel_id];
+$bot = new Bot($ctx, $d, $conv, $am);
 
-    $attachments = [];
-
-    if ($msg->attachments) {
-        foreach ($msg->attachments as $a) {
-            $asset = $am->downloadAsset($a->url);
-            array_push($attachments, $asset);
-        }
-    }
-
-    $embeds = [];
-
-    if ($msg->embeds) {
-        foreach ($msg->embeds as $e) {
-            $provider = $provider_url = null;
-
-            if ($e->provider) {
-                $provider = $e->provider->name ?? null;
-                $provider_url = $e->provider->url ?? null;
-            }
-
-            $embed_url = $e->video->url;
-            $asset_url = $embed_url ? null : $e->thumbnail->url;
-
-            $embed = new rolka\Embed(
-                -1,
-                $e->url,
-                $e->type == 'rich' ? 'link' : $e->type,
-                $e->color ? sprintf("#%06x", $e->color) : null,
-                $e->timestamp ? DateTimeImmutable::createFromMutable($e->timestamp) : null,
-                $provider,
-                $provider_url,
-                $e->footer?->text,
-                $e->footer?->icon_url,
-                $e->author?->name,
-                $e->author?->url,
-                $e->title,
-                $e->url,
-                $e->description,
-                $asset_url ? $am->downloadAsset($asset_url) : null,
-                $embed_url
-            );
-            array_push($embeds, $embed);
-        }
-    }
-
-    if (!($author = $ctx->getAuthor($msg->author->id))) {
-        $author = new rolka\Author(
-            $msg->author->id,
-            $msg->author->displayname,
-            $am->downloadAsset($msg->author->avatar)
-        );
-        $ctx->insertAuthor($author);
-    }
-
-    $m = new rolka\Message(
-        $ch,
-        $msg->id,
-        $author,
-        DateTimeImmutable::createFromMutable($msg->timestamp),
-        $msg->content,
-        $msg->referenced_message?->id,
-        $msg->sticker_items?->first()?->id,
-        $ctx->insertAttachmentGroup($attachments),
-        $ctx->insertEmbedGroup($embeds),
-    );
-
-    // hack... should put stuff on a queue
-    foreach ($attachments as $a) {
-        $am->generateThumbnail($a->id);
-    }
-
-    $ch->insertMessage($m);
-};
-
-$onMsgCreate = function (Message $msg, Discord $d) use ($ctx, $mapped_ch, $mapInsertMessage)
-{
-    echo "{$msg->author->username}: {$msg->content}", PHP_EOL;
-
-    if (!isset($mapped_ch[$msg->channel_id])) {
-        return;
-    }
-
-    $mapInsertMessage($msg, $mapped_ch);
-};
-
-function fetchMessages(Channel $ch, $mapped_ch, ?int $last_id = null)
-{
-    error_log(__FUNCTION__ .": last id $last_id");
-    $opts = ['limit' => 100];
-    if ($last_id) {
-        $opts['before'] = (string)$last_id;
-    }
-
-    return Async\await($ch->getMessageHistory($opts));
-}
-
-$fetchWholeHistory = function (Discord $d, int $channel_id, $mapped_ch) use ($mapInsertMessage)
-{
-    $channel = $d->getChannel($channel_id);
-
-    $last_id = null;
-
-    while (true) {
-        $messages = fetchMessages($channel, $mapped_ch, $last_id);
-
-        if ($messages instanceof Collection) {
-            foreach ($messages as $msg) {
-                $mapInsertMessage($msg, $mapped_ch);
-            }
-            $last = $messages->last();
-            if (!$last) {
-                break;
-            }
-            $last_id = $last->id;
-        } else {
-            break;
-        }
-    }
-};
-
-$d->on('init', function (Discord $d) use ($onMsgCreate, $fetchWholeHistory, $mapped_ch) {
-    error_log("Hello World!");
-
-    $d->on(Event::MESSAGE_CREATE, $onMsgCreate);
-
-    foreach ($mapped_ch as $id => $channel) {
-        $fetchWholeHistory($d, $id, $mapped_ch);
-    }
-});
-
-$d->run();
+$bot->run();
