25ea6d8cfc63549f90e78d9772d51151a7075e4f
wermi <32251376+wermipls@users.noreply.github.com>
2023-11-16 17:06:56 +0100

    markdown headers

--- src/Markdown.php
+++ src/Markdown.php
@@ -26,7 +26,28 @@ class Markdown extends Parsedown
             'SpecialCharacter',
         ];
         $this->InlineTypes['|'] = ['Spoiler'];
-        $this->inlineMarkerList .= '|';
+        $this->InlineTypes['#'] = ['Header'];
+        $this->inlineMarkerList .= '|#';
+    }
+
+    protected function inlineHeader($excerpt)
+    {
+        if (preg_match('/^(##?#?) (.+)\n?/m', $excerpt['context'], $matches, PREG_OFFSET_CAPTURE)) {
+            $text = $matches[2][0];
+            return array(
+                'extent' => strlen($matches[0][0]),
+                'offset' => $matches[0][1],
+                'element' => array(
+                    'handler' => 'line',
+                    'name' => match ($matches[1][0]) {
+                        '#'   => 'h1',
+                        '##'  => 'h2',
+                        '###' => 'h3',
+                    },
+                    'text' => $text,
+                ),
+            );
+        }
     }
 
     protected function inlineMention($excerpt)
